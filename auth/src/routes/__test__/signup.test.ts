c9f1aa0cde72898666b592dabc7d2693e7529244
Yuba Raj Khadka <ubrajkhadka@gmail.com>
2020-12-11 01:43:54 +1100

    jest auth route unit testing

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,78 @@
+import { app } from '../../app';
+import request from 'supertest';
+
+it('should returns a 201 on successful signup', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+});
+
+it('should returns a 400 with an invalid email', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtestcom',
+      password: 'password'
+    })
+    .expect(400);
+});
+
+it('should returns a 400 with an invalid password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'pas'
+    })
+    .expect(400);
+});
+
+it('should returns a 400 with an missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com'
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'buchucom'
+    })
+    .expect(400);
+});
+
+it('should disallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(400);
+});
+
+it('should sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -43,7 +43,7 @@ router.post(
       jwt: userJwt
     };
 
-    res.status(201).send(existingUser);
+    res.status(200).send(existingUser);
   }
 );
 
