fcae898ba821c5e9fc65d184f984af3798925c67
Yuba Raj Khadka <ubrajkhadka@gmail.com>
2020-12-12 23:36:50 +1100

    ticket service and deployments setup

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -5,7 +5,6 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
-
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
@@ -15,12 +14,12 @@ const start = async () => {
       useUnifiedTopology: true,
       useCreateIndex: true
     });
-    console.log('connected to auth  db');
+    console.log('connected to tickets  db');
   } catch (err) {
     console.log(err.message);
   }
   app.listen(3000, () => {
-    console.log('auth service starting on port 3000');
+    console.log('tickets service starting on port 3000');
   });
 };
 
