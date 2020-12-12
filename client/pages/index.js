b0491f086d0b0f817ae6be53fc60bcffb0bd1b27
Yuba Raj Khadka <ubrajkhadka@gmail.com>
2020-12-12 14:57:29 +1100

    server side rendered react auth route  nextjs

---
+++ client/pages/index.js
@@ -0,0 +1,17 @@
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+    </div>
+  );
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
+};
+
+export default LandingPage;
