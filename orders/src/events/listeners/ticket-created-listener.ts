21dffbdcc8080350aa36663f07e97d6ee22fdbd3
Yuba Raj Khadka <ubrajkhadka@gmail.com>
2020-12-29 01:47:07 +1100

    version number setup

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -1,13 +1,13 @@
-import { Message } from 'node-nats-streaming';
-import { Subjects, Listener, TicketCreatedEvent } from '@buchutickets/common';
-import { Ticket } from '../../models/ticket';
-import { queueGroupName } from './que-group-name';
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, TicketCreatedEvent } from "@buchutickets/common";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./que-group-name";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queGroupName = queueGroupName;
 
-  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     const { id, title, price } = data;
     const ticket = Ticket.build({
       id,
